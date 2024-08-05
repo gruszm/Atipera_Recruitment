fed5ac905c0b7ae05c9092e186f27e76611b0567
gruszm <mariuszgruszczynski11@gmail.com>
2024-08-05 12:52:36 +0200

    Annotation changed for the 'fork' flag

--- src/main/java/pl/gruszm/atipera_recruitment/datatransferobjects/GithubRepository.java
+++ src/main/java/pl/gruszm/atipera_recruitment/datatransferobjects/GithubRepository.java
@@ -1,14 +1,14 @@
 package pl.gruszm.atipera_recruitment.datatransferobjects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GithubRepository
 {
     private String name;
     private Owner owner;
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private boolean fork;
     private GithubBranch[] branches;
 
